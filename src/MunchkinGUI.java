cf33271a029119474b0761e2793798159d58f796
wikihawki <grimo@poczta.onet.pl>
2015-12-30 17:06:52 +0100

    Gui

---
+++ src/MunchkinGUI.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class MunchkinGUI {
+
+	private objCreateAppletImage createImage = new objCreateAppletImage();
+	private spiderSolitareWindow mainWindow;
+	private MediaTracker mt;
+	private String[] strCardBack = {"Sonic","BelgiumFlag","DutchMouse","FindingNemo","UKFlag","VanessaMae"};
+	
+	
+	
+}
